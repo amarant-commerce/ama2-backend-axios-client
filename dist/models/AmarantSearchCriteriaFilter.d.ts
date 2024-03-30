9617b1e0ff10e72a0b6a831095331d30d73458bd
Ivan Rogar <ivan.rogar@amarant-commerce.com>
2024-03-30 21:29:12 +0100

    UPDATED client

--- dist/models/AmarantSearchCriteriaFilter.d.ts
+++ dist/models/AmarantSearchCriteriaFilter.d.ts
@@ -3,4 +3,8 @@ export type AmarantSearchCriteriaFilter = {
         condition?: 'eq' | 'neq' | 'in' | 'nin' | 'gt' | 'lt' | 'lte' | 'gte' | 'null' | 'range' | 'like';
         value?: number | number | string | boolean | any[] | null;
     }>>;
+    sort?: Array<{
+        path?: string;
+        direction?: 'ASC' | 'DESC';
+    }>;
 };
--- src/models/AmarantSearchCriteriaFilter.ts
+++ src/models/AmarantSearchCriteriaFilter.ts
@@ -7,5 +7,9 @@ export type AmarantSearchCriteriaFilter = {
         condition?: 'eq' | 'neq' | 'in' | 'nin' | 'gt' | 'lt' | 'lte' | 'gte' | 'null' | 'range' | 'like';
         value?: number | number | string | boolean | any[] | null;
     }>>;
+    sort?: Array<{
+        path?: string;
+        direction?: 'ASC' | 'DESC';
+    }>;
 };
 
