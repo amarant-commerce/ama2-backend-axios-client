b8a59a333ce7500bde8f0896eb15e9be7e809ff9
Ivan Rogar <ivan.rogar@amarant-commerce.com>
2024-04-06 14:45:10 +0200

    FIXED array params UPDATED generator

--- dist/core/request.js
+++ dist/core/request.js
@@ -48,9 +48,12 @@ export const getQueryString = (params) => {
     const process = (key, value) => {
         if (isDefined(value)) {
             if (Array.isArray(value)) {
-                value.forEach(v => {
-                    process(key, v);
+                let newValue = {};
+                value.forEach((i, v) => {
+                    newValue[i] = v;
                 });
+                console.log(newValue);
+                process(key, newValue);
             }
             else if (typeof value === 'object') {
                 Object.entries(value).forEach(([k, v]) => {
